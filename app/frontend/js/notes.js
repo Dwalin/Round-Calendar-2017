7800a46317775835120e181c5edcbfbb362e72bd
Oles <ibolosig@gmail.com>
2017-04-11 04:19:13 +0300

    Counters.

--- app/frontend/js/notes.js
+++ app/frontend/js/notes.js
@@ -45,34 +45,6 @@ module.exports = function (calendar, notes) {
 
 		note.text(noteData.note);
 
-		//if (noteData.counter != undefined) {
-		//
-		//	var parsed = JSON.parse(noteData.counter);
-		//
-		//	console.log(parsed);
-		//
-		//	for (key in parsed) {
-		//		var note = day.append("g")
-		//			.append("circle")
-		//			.attr("r", "0.5")
-		//			.attr("cx", "-50")
-		//			.attr('transform',
-		//			function() {
-		//				if (noteData.day < 182) {
-		//					return 'rotate(' + ( (noteData.day) * (0.9863) + 270) + ', 0, 0)';
-		//				} else {
-		//					return 'rotate(' + ( (noteData.day) * (0.9863) + 90) + ', 0, 0)';
-		//				}
-		//			})
-		//			.attr("data-value", parsed[key])
-		//			.classed('mj-calendar__counter', true);
-		//
-		//
-		//	}
-		//}
-
-		//console.log(noteData);
-
 	});
 
 };
