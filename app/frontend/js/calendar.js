5f9d3a0274d452eb9458482aa9f711573c34fe11
Oles <ibolosig@gmail.com>
2017-04-11 04:14:48 +0300

    Counters.

--- app/frontend/js/calendar.js
+++ app/frontend/js/calendar.js
@@ -343,7 +343,13 @@ $(function() {
 
 	ko.bindingHandlers.note = {
 		init: function(element, valueAccessor) {
+			$(document).on("click", ".mj-calendar__circle", function(){
 
+				//console.log("Note adding popup.");
+
+				$(".js-popup__note").removeClass("js-hidden");
+				$(element).val( $(this).parent().attr("data-note") );
+			});
 		}
 	};
 
