b53b73956cd9cb7b92038cd7a050cef37f8b72dd
Oles <ibolosig@gmail.com>
2017-04-11 04:00:05 +0300

    Counters.

--- dist/js/calendar.js
+++ dist/js/calendar.js
@@ -159,7 +159,7 @@ $(function() {
 		self.validate = function(email) {
 			var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 			return re.test(email);
-		};gulp
+		};
 		self.refresh = function() {
 
 			$.ajax({
